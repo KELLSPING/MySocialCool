bf5411b52ea4bad3a78bf67bf68a95c9a0596c6e
kells\KellsPing <kellsping@gmail.com>
2023-12-17 20:46:31 +0800

    Setup firebase config.

---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1NfV5tf3dpJRc0Tx0Sko7lcK4mRDkbEQ",
+  authDomain: "social-cool-bd30a.firebaseapp.com",
+  projectId: "social-cool-bd30a",
+  storageBucket: "social-cool-bd30a.appspot.com",
+  messagingSenderId: "519215517006",
+  appId: "1:519215517006:web:585a545018597a3e92a21f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
