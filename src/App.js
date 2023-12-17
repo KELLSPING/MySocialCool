3d21a542b8533a1863b09cf64cc23f6c0c8e848c
kells\KellsPing <kellsping@gmail.com>
2023-12-17 19:41:33 +0800

    Create "pages" folder.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './Header';
-import Home from './Home';
-import SignIn from './SignIn';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
