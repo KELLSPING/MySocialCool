e423568da06060fb5b31be4b86a231d1d8abb899
kells\KellsPing <kellsping@gmail.com>
2023-12-17 20:00:54 +0800

    Update view.
    Add form input and form button.

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,15 +2,32 @@ import React from "react";
 import { Menu, Form, Container } from "semantic-ui-react";
 
 function SignIn() {
-    const [activeItem, setActiveItem] = React.useState('signin');
+  const [activeItem, setActiveItem] = React.useState("signin");
 
   return (
     <Container>
       <Menu widths={2}>
-        <Menu.Item active={activeItem === 'register'} onClick={() => setActiveItem('register')}>註冊</Menu.Item>
-        <Menu.Item active={activeItem === 'signin'} onClick={() => setActiveItem('signin')}>登入</Menu.Item>
+        <Menu.Item
+          active={activeItem === "register"}
+          onClick={() => setActiveItem("register")}
+        >
+          註冊
+        </Menu.Item>
+        <Menu.Item
+          active={activeItem === "signin"}
+          onClick={() => setActiveItem("signin")}
+        >
+          登入
+        </Menu.Item>
       </Menu>
-      <Form></Form>
+      <Form>
+        <Form.Input label="信箱" value="" placeholder="請輸入信箱" />
+        <Form.Input label="密碼" value="" placeholder="請輸入密碼" />
+        <Form.Button>
+            {activeItem === 'register' && '註冊'}
+            {activeItem === 'signin' && '登入'}
+        </Form.Button>
+      </Form>
     </Container>
   );
 }
