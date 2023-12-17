91c9b7653e9c1584ef9b30c6b668d2eb74cefb91
kells\KellsPing <kellsping@gmail.com>
2023-12-17 10:58:50 +0800

    Updat view.
    Add item in the menu bar.

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,15 @@
-import { Menu } from "semantic-ui-react";
+import { Menu, Search } from "semantic-ui-react";
 
 function Header() {
-    return <Menu/>
+    return <Menu>
+        <Menu.Item>Social Cool</Menu.Item>
+        <Menu.Item>
+            <Search></Search>
+        </Menu.Item>
+        <Menu.Menu position="right">
+            <Menu.Item>註冊/登入</Menu.Item>
+        </Menu.Menu>
+    </Menu>
 }
 
 export default Header;
