713f36bc8c60ca0320b331070dfc58a5150b6912
kells\KellsPing <kellsping@gmail.com>
2023-12-17 21:06:44 +0800

    Add function.
    Register and signin successfully.

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,51 @@
 import React from "react";
 import { Menu, Form, Container } from "semantic-ui-react";
 
+import firebase from "../utils/firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+import { useNavigate } from "react-router-dom";
+
 function SignIn() {
+  const navigate = useNavigate();
   const [activeItem, setActiveItem] = React.useState("signin");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+
+  function onSubmit() {
+    if (activeItem === "register") {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+          navigate("/");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+    } else if (activeItem === "signin") {
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+          navigate("/");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
+  }
 
   return (
     <Container>
@@ -22,7 +63,7 @@ function SignIn() {
           登入
         </Menu.Item>
       </Menu>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Form.Input
           label="信箱"
           value={email}
