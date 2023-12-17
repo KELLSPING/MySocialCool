6f77e2d1699a210abe9123e6c5647038a38312e7
kells\KellsPing <kellsping@gmail.com>
2023-12-17 19:56:49 +0800

    Update view.
    Add register and signin button.
    Add register and signin button onclick.

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,18 @@
+import React from "react";
+import { Menu, Form, Container } from "semantic-ui-react";
+
 function SignIn() {
-    return "SignIn"
+    const [activeItem, setActiveItem] = React.useState('signin');
+
+  return (
+    <Container>
+      <Menu widths={2}>
+        <Menu.Item active={activeItem === 'register'} onClick={() => setActiveItem('register')}>註冊</Menu.Item>
+        <Menu.Item active={activeItem === 'signin'} onClick={() => setActiveItem('signin')}>登入</Menu.Item>
+      </Menu>
+      <Form></Form>
+    </Container>
+  );
 }
 
 export default SignIn;
