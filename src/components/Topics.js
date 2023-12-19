5fcd18d0b2a0b8fa4724f6bf1f754208e10692e9
Kells_P <kellsping@gmail.com>
2023-12-20 07:14:04 +0800

    Create file and update view.
    Use "Topics" to change the view in the "Post".

---
+++ src/components/Topics.js
@@ -0,0 +1,5 @@
+function Topics() {
+    return "Topics";
+}
+
+export default Topics;
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,10 +1,12 @@
 import { Grid } from "semantic-ui-react";
 
+import Topics from "../components/Topics";
+
 function Posts() {
   return (
     <Grid>
       <Grid.Row>
-        <Grid.Column width={3}>主題列表</Grid.Column>
+        <Grid.Column width={3}><Topics /></Grid.Column>
         <Grid.Column width={10}>貼文列表</Grid.Column>
         <Grid.Column width={3}>空白</Grid.Column>
       </Grid.Row>
