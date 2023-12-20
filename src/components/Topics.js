aece539e81a8d560214b54dad12ffc852b8c5821
Kells_P <kellsping@gmail.com>
2023-12-20 17:27:27 +0800

    Get a document from firebase.

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,5 +1,26 @@
+import React from "react";
+import app from "../utils/firebase";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+
 function Topics() {
-    return "Topics";
+  React.useEffect(() => {
+    async function fetchData() {
+      const db = getFirestore(app);
+
+      const docRef = doc(db, "topics", "movie");
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }
+    fetchData();
+  }, []);
+
+  return "Topics";
 }
 
 export default Topics;
