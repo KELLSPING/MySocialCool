9307766a29c0ca6571e66439b1ee952d2fd623e1
Kells_P <kellsping@gmail.com>
2023-12-20 18:31:25 +0800

    Update view.
    Show text in new post page.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import Posts from './pages/Posts';
+import NewPost from './pages/NewPost';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Posts />}/>
         <Route exact path='/signin' element={<SignIn />}/>
+        <Route exact path='/newpost' element={<NewPost />}/>
       </Routes>
     </Router>
   );
