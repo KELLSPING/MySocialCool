642c21553b2bbe15188fe8fbeec724fc90c60976
Kells_P <kellsping@gmail.com>
2023-12-20 18:14:25 +0800

    (Update) Get multiple documents from a collection in firebase.

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -6,6 +6,7 @@ import {
   query,
   where,
   getDocs,
+  doc,
 } from "firebase/firestore";
 
 function Topics() {
@@ -16,10 +17,10 @@ function Topics() {
       const q = query(collection(db, "topics"));
 
       const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+      const data = querySnapshot.docs.map((doc) => {
+        return doc.data();
       });
+      console.log(data);
     }
     fetchData();
   }, []);
